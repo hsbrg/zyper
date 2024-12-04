cebcfb6220485c80a1f71298f28e0a52528edecd
aky6 <akash@zoftwarehub.com>
2024-12-04 19:02:26 +0530

    new dist

--- dist/zyper2/server/main.server.mjs
+++ dist/zyper2/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-D7BUUCRT.mjs";import"./chunk-46RB6P4B.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-XUFI5VWH.mjs";import"./chunk-46RB6P4B.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
