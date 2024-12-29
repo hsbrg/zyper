26899002d1265e88b610c130641ddd367a314bcd
aky6 <akash@zoftwarehub.com>
2024-12-29 22:19:58 +0530

    new build

--- dist/zyper2/server/main.server.mjs
+++ dist/zyper2/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SHGQGYUQ.mjs";import"./chunk-C7MVLQA5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-DBGQWIUO.mjs";import"./chunk-C7MVLQA5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
